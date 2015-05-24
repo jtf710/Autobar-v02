6084b38f8b0f3da5221b019f8a1a6cc8e5333855
jtf710 <jfish@jfishs-MacBook-Pro.local>
2015-05-23 22:02:39 -0400

    back to basics

--- Autobar v02/ViewController.swift
+++ Autobar v02/ViewController.swift
@@ -62,47 +62,8 @@ class ViewController: UIViewController {
         }
 
     }
+    
 
-    
-    
-    
-    func colorButtonsView(buttonSize:CGSize, buttonCount:Int) -> UIView {
-        //creates color buttons in a UIView
-        let buttonView = UIView()
-        buttonView.backgroundColor = UIColor.blackColor()
-        buttonView.frame.origin = CGPointMake(0,0)
-        
-        let padding = CGSizeMake(10, 10)
-        buttonView.frame.size.width = (buttonSize.width + padding.width) * CGFloat(buttonCount)
-        buttonView.frame.size.height = (buttonSize.height +  2.0 * padding.height)
-
-        //add buttons to the view
-        var buttonPosition = CGPointMake(padding.width * 0.5, padding.height)
-        let buttonIncrement = buttonSize.width + padding.width
-        let hueIncrement = 1.0 / CGFloat(buttonCount)
-        var newHue = hueIncrement
-        
-        for i in 0...(buttonCount - 1)  {
-            var button = UIButton.buttonWithType(.Custom) as UIButton
-            button.frame.size = buttonSize
-            button.frame.origin = buttonPosition
-            buttonPosition.x = buttonPosition.x + buttonIncrement
-            button.backgroundColor = UIColor(hue: newHue, saturation: 1.0, brightness: 1.0, alpha: 1.0)
-            newHue = newHue + hueIncrement
-            button.addTarget(self, action: "colorButtonPressed:", forControlEvents: .TouchUpInside)
-            buttonView.addSubview(button)
-        }
-        
-        func colorButtonPressed(sender:UIButton){
-            buttonScroll.backgroundColor = sender.backgroundColor
-        }
-        
-        
-        return buttonView
-    }
-    
-    
-    
     
     
     override func viewDidLoad() {
@@ -110,18 +71,7 @@ class ViewController: UIViewController {
         
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
-        //scrolling pageview
-        let scrollingView = colorButtonsView(CGSizeMake(100.0,50.0), buttonCount: 10)
-        buttonScroll.contentSize = scrollingView.frame.size
-        buttonScroll.addSubview(scrollingView)
-        buttonScroll.showsHorizontalScrollIndicator = true
-        buttonScroll.indicatorStyle = .Default
-        
-        
-        
-        
+                
         
     }
 
